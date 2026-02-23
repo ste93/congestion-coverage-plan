a58f9c9e000a0922219f12cde611776e97e0e7f2
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2026-02-23 16:51:44 +0100

    minor updates to docker
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -3,4 +3,4 @@
 # This script builds a Docker image for the application.
 
 
-docker build -t ste93/congestion-coverage-plan:latest . --build-arg BRANCH=fix/library
+docker build -t ste93/congestion-coverage-plan:latest . --build-arg BRANCH=fix/UC3_rebased --build-arg REPO-USERNAME=ste93
